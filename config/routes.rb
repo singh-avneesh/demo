7fc4271746dc148c026fc96facdb00ecf6b494ae
s-avneesh <avneesh.2916@gmail.com>
2017-06-11 12:23:15 +0530

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users
   resources :index
-   root "users#new" 
+  resources :show   
+  root "users#new" 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
