e4f368b808cbcef190887aaaee45169688abb19a
s-avneesh <avneesh.2916@gmail.com>
2017-06-11 12:21:44 +0530

    routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  skip_before_filter:new_users,:only[:show,:update]
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :users
   resources :index
-   root "users#new" 
+  resources :show
+  resources :delete
+  root "users#new" 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
