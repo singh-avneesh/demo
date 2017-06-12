0fa28556df7c5c0bd4ca06d09020231f6c6e7773
s-avneesh <avneesh.2916@gmail.com>
2017-06-12 11:02:49 +0530

    payment

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :index
   resources :show
   resources :delete
-  root "users#new" 
+  root "payment#new" 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
